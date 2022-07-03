077cd752c8adb467c475887008e43433f4eae0ff
luisdiego18 <diegotapia1987@gmail.com>
2022-07-03 15:18:42 -0500

    Users

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+// Schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// Model
+const User = mongoose.model("User", userSchema);
+
+// Joi Validation
+function validateUser(user) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+  return schema.validate(user, {
+    abortEarly: false,
+  });
+}
+
+exports.User = User;
+exports.validate = validateUser;
--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const home = require("../routers/home");
 const jobs = require("../routers/jobs");
+const users = require("../routers/users");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/", home);
   app.use("/api/jobs", jobs);
+  app.use("/api/users", users);
 };
